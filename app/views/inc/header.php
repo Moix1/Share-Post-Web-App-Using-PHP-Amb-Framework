0e8116fceaa1157d3206390aa7aa20c010e6fce3
Moix1 <abdulmoix1@outlook.com>
2019-01-04 12:39:48 +0500

    Initial Commit

---
+++ app/views/inc/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title><?php echo SITENAME; ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo URLROOT; ?>/css/style.css">
+</head>
+<body>
+    <?php require APPROOT . '/views/inc/navbar.php'; ?>
+    <div class="container">
+
+    
